3b736574ba65d906d75d37b97bec5a9924dab1ae
Rakhal01234 <154105700+Rakhal01234@users.noreply.github.com>
2024-07-11 08:39:49 +0530

    Update
    krusckals algo updated

---
+++ krusckals.c
@@ -0,0 +1,90 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define V 5
+
+typedef struct {
+    int src, dest, weight;
+} Edge;
+
+typedef struct {
+    int parent, rank;
+} Subset;
+
+int compare(const void* a, const void* b) {
+    Edge* a1 = (Edge*)a;
+    Edge* b1 = (Edge*)b;
+    return a1->weight > b1->weight;
+}
+
+int find(Subset subsets[], int i) {
+    if (subsets[i].parent != i)
+        subsets[i].parent = find(subsets, subsets[i].parent);
+    return subsets[i].parent;
+}
+
+void Union(Subset subsets[], int x, int y) {
+    int xroot = find(subsets, x);
+    int yroot = find(subsets, y);
+    if (subsets[xroot].rank < subsets[yroot].rank)
+        subsets[xroot].parent = yroot;
+    else if (subsets[xroot].rank > subsets[yroot].rank)
+        subsets[yroot].parent = xroot;
+    else {
+        subsets[yroot].parent = xroot;
+        subsets[xroot].rank++;
+    }
+}
+
+void KruskalMST(Edge edges[], int E) {
+    Edge result[V];
+    int e = 0;
+    int i = 0;
+
+    qsort(edges, E, sizeof(edges[0]), compare);
+
+    Subset* subsets = (Subset*)malloc(V * sizeof(Subset));
+    for (int v = 0; v < V; ++v) {
+        subsets[v].parent = v;
+        subsets[v].rank = 0;
+    }
+
+    while (e < V - 1 && i < E) {
+        Edge next_edge = edges[i++];
+        int x = find(subsets, next_edge.src);
+        int y = find(subsets, next_edge.dest);
+
+        if (x != y) {
+            result[e++] = next_edge;
+            Union(subsets, x, y);
+        }
+    }
+
+    printf("Following are the edges in the constructed MST\n");
+    for (i = 0; i < e; ++i)
+        printf("%d -- %d == %d\n", result[i].src, result[i].dest, result[i].weight);
+    return;
+}
+
+int main() {
+    int graph[V][V] = {{0, 2, 0, 6, 0},
+                       {2, 0, 3, 8, 5},
+                       {0, 3, 0, 0, 7},
+                       {6, 8, 0, 0, 9},
+                       {0, 5, 7, 9, 0}};
+    Edge edges[9];
+    int k = 0;
+    for (int i = 0; i < V; i++) {
+        for (int j = i + 1; j < V; j++) {
+            if (graph[i][j]) {
+                edges[k].src = i;
+                edges[k].dest = j;
+                edges[k].weight = graph[i][j];
+                k++;
+            }
+        }
+    }
+
+    KruskalMST(edges, k);
+    return 0;
+}
